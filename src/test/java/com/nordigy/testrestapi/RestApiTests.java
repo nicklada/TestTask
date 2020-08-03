77c29d726568a1a4d69006024e07ffbe06510a0e
lada <nicklada94@gmail.com>
2020-08-03 17:56:36 +0300

    Tests created

--- src/test/java/com/nordigy/testrestapi/RestApiTests.java
+++ src/test/java/com/nordigy/testrestapi/RestApiTests.java
@@ -35,35 +35,13 @@ class RestApiTests {
         RestAssured.port = port;
     }
 
-    @Nested
-    @DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
-    public class GetMethodTests {
-
-//        @Test
-//        public void shouldReturnCorrectUsersListSize() {
-//            given().log().all()
-//                    .when().get("/api/users")
-//                    .then().log().ifValidationFails()
-//                    .statusCode(200)
-//                    .body("page.totalElements", is(20));
-//        }
-//
-//        @Test
-//        public void shouldReturnCorrectPageSize() {
-//            given().log().all()
-//                    .when().get("/api/users")
-//                    .then().log().ifValidationFails()
-//                    .statusCode(200)
-//                    .body("page.size", is(20));
-//        }
-
         @Test
-        public void shouldReturnCorrectNumberOfPages() {
+        public void shouldReturnCorrectUsersListSize() {
             given().log().all()
                     .when().get("/api/users")
                     .then().log().ifValidationFails()
                     .statusCode(200)
-                    .body("page.totalPages", is(1));
+                    .body("page.totalElements", is(20));
         }
 
         @Test
@@ -129,308 +107,19 @@ class RestApiTests {
                     .statusCode(200)
                     .body(("_embedded.users.firstName.get(0)"), is("Яяяяяяяяяяяяяяя"));
         }
-    }
-
-    @Nested
-    @DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
-    public class PostMethodTests {
 
         @Test
-        public void shouldNotCreateNewUserWhenEmptyFields() {
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectNode objectNode = objectMapper.createObjectNode();
-            objectNode.put("firstName", "");
-            objectNode.put("lastName", "");
-            objectNode.put("dayOfBirth", "");
-            objectNode.put("email", "");
-
+        public void shouldDeleteExistingUser() {
             given().log().all()
-                    .body(objectNode)
-                    .contentType(ContentType.JSON)
-                    .when().post("/api/users")
+                    .when().delete("/api/users/1")
                     .then().log().ifValidationFails()
-                    .statusCode(400);
-        }
-
-        @Test
-        public void shouldCreateNewUserWhenName2Symbols() {
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectNode objectNode = objectMapper.createObjectNode();
-            objectNode.put("firstName", "El");
-            objectNode.put("lastName", "Ivanov");
-            objectNode.put("dayOfBirth", "2000-01-01");
-            objectNode.put("email", "asdas@asdas.ru");
-
-            ObjectNode user = given().log().all()
-                    .body(objectNode)
-                    .contentType(ContentType.JSON)
-                    .when().post("/api/users")
-                    .then().log().ifValidationFails()
-                    .statusCode(201)
-                    .extract().body().as(ObjectNode.class);
-
-            assertThat(user.get("id").asLong()).isGreaterThan(20);
-            assertThat(user.get("firstName")).isEqualTo(objectNode.get("firstName"));
-            assertThat(user.get("lastName")).isEqualTo(objectNode.get("lastName"));
-            assertThat(user.get("dayOfBirth")).isEqualTo(objectNode.get("dayOfBirth"));
-            assertThat(user.get("email")).isEqualTo(objectNode.get("email"));
-        }
-
-        @Test
-        public void shouldCreateNewUserWhenName15Symbols() {
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectNode objectNode = objectMapper.createObjectNode();
-            objectNode.put("firstName", "Алёна-Генриэтта");
-            objectNode.put("lastName", "Ivanov");
-            objectNode.put("dayOfBirth", "2000-01-01");
-            objectNode.put("email", "tgkdl@asdas.ru");
-
-            ObjectNode user = given().log().all()
-                    .body(objectNode)
-                    .contentType(ContentType.JSON)
-                    .when().post("/api/users")
-                    .then().log().ifValidationFails()
-                    .statusCode(201)
-                    .extract().body().as(ObjectNode.class);
-
-            assertThat(user.get("id").asLong()).isGreaterThan(20);
-            assertThat(user.get("firstName")).isEqualTo(objectNode.get("firstName"));
-            assertThat(user.get("lastName")).isEqualTo(objectNode.get("lastName"));
-            assertThat(user.get("dayOfBirth")).isEqualTo(objectNode.get("dayOfBirth"));
-            assertThat(user.get("email")).isEqualTo(objectNode.get("email"));
-        }
-
-        @Test
-        public void shouldNotCreateNewUserWhenName1Symbol() {
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectNode objectNode = objectMapper.createObjectNode();
-            objectNode.put("firstName", "I");
-            objectNode.put("lastName", "Ivanov");
-            objectNode.put("dayOfBirth", "2000-01-01");
-            objectNode.put("email", "alyona@asdas.ru");
-
+                    .statusCode(204);
             given().log().all()
-                    .body(objectNode)
-                    .contentType(ContentType.JSON)
-                    .when().post("/api/users")
-                    .then().log().ifValidationFails()
-                    .statusCode(400)
-                    .body("subErrors.get(0).message", is("размер должен находиться в диапазоне от 2 до 15"));
-        }
-
-        @Test
-        public void shouldNotCreateNewUserWhenName16Symbols() {
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectNode objectNode = objectMapper.createObjectNode();
-            objectNode.put("firstName", "Ibhjllkjhgfdddrt");
-            objectNode.put("lastName", "Ivanov");
-            objectNode.put("dayOfBirth", "2000-01-01");
-            objectNode.put("email", "alyona@asdas.ru");
-
-            given().log().all()
-                    .body(objectNode)
-                    .contentType(ContentType.JSON)
-                    .when().post("/api/users")
-                    .then().log().ifValidationFails()
-                    .statusCode(400)
-                    .body("subErrors.get(0).message", is("размер должен находиться в диапазоне от 2 до 15"));
-        }
-
-        @Test
-        public void shouldNotCreateNewUserWhenSurname1Symbol() {
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectNode objectNode = objectMapper.createObjectNode();
-            objectNode.put("firstName", "Ivan");
-            objectNode.put("lastName", "I");
-            objectNode.put("dayOfBirth", "2000-01-01");
-            objectNode.put("email", "alyona@asdas.ru");
-
-            given().log().all()
-                    .body(objectNode)
-                    .contentType(ContentType.JSON)
-                    .when().post("/api/users")
-                    .then().log().ifValidationFails()
-                    .statusCode(400)
-                    .body("subErrors.get(0).message", is("размер должен находиться в диапазоне от 2 до 30"));
-        }
-
-        @Test
-        public void shouldNotCreateNewUserWhenSurname31Symbols() {
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectNode objectNode = objectMapper.createObjectNode();
-            objectNode.put("firstName", "Ivan");
-            objectNode.put("lastName", "Киселёв-ЛопатинКиселёв-Лопатины");
-            objectNode.put("dayOfBirth", "2000-01-01");
-            objectNode.put("email", "alyona@asdas.ru");
-
-            given().log().all()
-                    .body(objectNode)
-                    .contentType(ContentType.JSON)
-                    .when().post("/api/users")
-                    .then().log().ifValidationFails()
-                    .statusCode(400)
-                    .body("subErrors.get(0).message", is("размер должен находиться в диапазоне от 2 до 30"));
-        }
-
-        @Test
-        public void shouldNotCreateNewUserWhenEmailInvalid() {
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectNode objectNode = objectMapper.createObjectNode();
-            objectNode.put("firstName", "Ivan");
-            objectNode.put("lastName", "Ivanov");
-            objectNode.put("dayOfBirth", "2000-01-01");
-            objectNode.put("email", "!*)(*&^54678jhgf");
-
-            given().log().all()
-                    .body(objectNode)
-                    .contentType(ContentType.JSON)
-                    .when().post("/api/users")
-                    .then().log().ifValidationFails()
-                    .statusCode(400)
-                    .body("subErrors.get(0).message", is("должно иметь формат адреса электронной почты"));
-        }
-
-        @Test
-        public void shouldNotCreateNewUserWhenEmailAlreadyExists() {
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectNode existingUser = given().log().all()
-                    .contentType(ContentType.JSON)
-                    .when().get("/api/users/5")
+                    .when().get("/api/users")
                     .then().log().ifValidationFails()
                     .statusCode(200)
-                    .extract().body().as(ObjectNode.class);
-
-            ObjectNode objectNode = objectMapper.createObjectNode();
-            objectNode.put("firstName", "Ivan");
-            objectNode.put("lastName", "Ivanov");
-            objectNode.put("dayOfBirth", "2000-01-01");
-            objectNode.put("email", existingUser.get("email"));
-
-            given().log().all()
-                    .body(objectNode)
-                    .contentType(ContentType.JSON)
-                    .when().post("/api/users")
-                    .then().log().ifValidationFails()
-                    .statusCode(409)
-                    .body("message", is("Database error"))
-                    .body("debugMessage", containsStringIgnoringCase("Нарушение уникального индекса или первичного ключа"));
+                    .body("page.totalElements", is(19));
         }
-
-        @Test
-        public void shouldNotCreateNewUserWhenDateInvalidFormat() {
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectNode objectNode = objectMapper.createObjectNode();
-            objectNode.put("firstName", "Ivan");
-            objectNode.put("lastName", "Ivanov");
-            objectNode.put("dayOfBirth", "2000.01.01");
-            objectNode.put("email", "kjhgh@asdas.ru");
-
-            given().log().all()
-                    .body(objectNode)
-                    .contentType(ContentType.JSON)
-                    .when().post("/api/users")
-                    .then().log().ifValidationFails()
-                    .statusCode(400)
-                    .body("message", is("Wrong content-type of the request format. Expected content-type is application/json."))
-                    .body("debugMessage", containsStringIgnoringCase("JSON parse error: Cannot deserialize value of type `java.time.LocalDate` from String"));
-        }
-
-        @Test
-        public void shouldNotCreateNewUserWhenDateInFuture() {
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectNode objectNode = objectMapper.createObjectNode();
-            objectNode.put("firstName", "Ivan");
-            objectNode.put("lastName", "Ivanov");
-            objectNode.put("dayOfBirth", "3000-01-01");
-            objectNode.put("email", "kjhgh@asdas.ru");
-
-            given().log().all()
-                    .body(objectNode)
-                    .contentType(ContentType.JSON)
-                    .when().post("/api/users")
-                    .then().log().ifValidationFails()
-                    .statusCode(400)
-                    .body("message", is("Validation failed"))
-                    .body("subErrors.get(0).message", is("должно содержать прошедшую дату"));
-        }
-    }
-
-    @Nested
-    @DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
-    public class PutMethodTests {
-
-        @Test
-        public void shouldPutNewInfoToExistingUser() {
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectNode objectNode = objectMapper.createObjectNode();
-            objectNode.put("firstName", "Julia");
-            objectNode.put("lastName", "Smith");
-            objectNode.put("dayOfBirth", "1967-01-01");
-            objectNode.put("email", "juli@asdas.ru");
-
-            ObjectNode user = given().log().all()
-                    .body(objectNode)
-                    .contentType(ContentType.JSON)
-                    .when().put("/api/users/3")
-                    .then().log().ifValidationFails()
-                    .statusCode(200)
-                    .extract().body().as(ObjectNode.class);
-
-            assertThat(user.get("id").asInt()).isEqualTo(3);
-            assertThat(user.get("firstName")).isEqualTo(objectNode.get("firstName"));
-            assertThat(user.get("lastName")).isEqualTo(objectNode.get("lastName"));
-            assertThat(user.get("dayOfBirth")).isEqualTo(objectNode.get("dayOfBirth"));
-            assertThat(user.get("email")).isEqualTo(objectNode.get("email"));
-        }
-
-        @Test
-        public void shouldNotPutNewInfoToExistingUserWhenSomeFieldsEmpty() {
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectNode objectNode = objectMapper.createObjectNode();
-            objectNode.put("firstName", "Mari");
-            objectNode.put("lastName", "Ldjkgk");
-            objectNode.put("dayOfBirth", "");
-            objectNode.put("email", "fjgkgk@gdkd.com");
-
-            given().log().all()
-                    .body(objectNode)
-                    .contentType(ContentType.JSON)
-                    .when().put("/api/users/2")
-                    .then().log().ifValidationFails()
-                    .statusCode(400)
-                    .body("message", is("Validation failed"))
-                    .body("subErrors.get(0).message", is("не должно равняться null"));
-        }
-
-    @Test
-    public void shouldCreateNewUserWhenPutInfoToNotExistingUser() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        ObjectNode objectNode = objectMapper.createObjectNode();
-        objectNode.put("firstName", "Mari");
-        objectNode.put("lastName", "Brown");
-        objectNode.put("dayOfBirth", "1989-03-02");
-        objectNode.put("email", "mari@gjgj.com");
-
-        ObjectNode user = given().log().all()
-                .body(objectNode)
-                .contentType(ContentType.JSON)
-                .when().put("/api/users/51")
-                .then().log().ifValidationFails()
-                .statusCode(201)
-                .extract().body().as(ObjectNode.class);
-
-        assertThat(user.get("firstName")).isEqualTo(objectNode.get("firstName"));
-        assertThat(user.get("lastName")).isEqualTo(objectNode.get("lastName"));
-        assertThat(user.get("dayOfBirth")).isEqualTo(objectNode.get("dayOfBirth"));
-        assertThat(user.get("email")).isEqualTo(objectNode.get("email"));
-
-
-    }
-    }
-
-    @Nested
-    @DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
-    public class PatchMethodTests {
 
         @Test
         public void shouldPatchNewInfoToExistingUser() {
@@ -461,52 +150,6 @@ class RestApiTests {
             assertThat(userAfterPatch.get("dayOfBirth")).isEqualTo(userBeforePatch.get("dayOfBirth"));
             assertThat(userAfterPatch.get("email")).isEqualTo(objectNode.get("email"));
         }
-
-        @Test
-        public void shouldNotPatchNewInfoToNotExistingUser() {
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectNode objectNode = objectMapper.createObjectNode();
-            objectNode.put("firstName", "Mari");
-            objectNode.put("lastName", "");
-            objectNode.put("dayOfBirth", "");
-            objectNode.put("email", "");
-
-            given().log().all()
-                    .body(objectNode)
-                    .contentType(ContentType.JSON)
-                    .when().patch("/api/users/55")
-                    .then().log().ifValidationFails()
-                    .statusCode(404);
-        }
-    }
-
-    @Nested
-    @DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
-    public class DeleteMethodTests {
-
-        @Test
-        public void shouldDeleteExistingUser() {
-            given().log().all()
-                    .when().delete("/api/users/1")
-                    .then().log().ifValidationFails()
-                    .statusCode(204);
-            given().log().all()
-                    .when().get("/api/users")
-                    .then().log().ifValidationFails()
-                    .statusCode(200)
-                    .body("page.totalElements", is(19));
-
-        }
-
-        @Test
-        public void shouldDoNothingWhenDeleteNotExistingUser() {
-            given().log().all()
-                    .when().delete("/api/users/40")
-                    .then().log().ifValidationFails()
-                    .statusCode(404);
-        }
-    }
-
     // TODO: The test methods above are examples of test cases.
     //  Please add new cases below, but don't hesitate to refactor the whole class.
 }
