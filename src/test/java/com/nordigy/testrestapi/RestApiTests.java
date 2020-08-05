adebe68ca57c6525d617175353616da040352d59
lada <nicklada94@gmail.com>
2020-08-05 15:18:10 +0300

    Smoke tests updated

--- src/test/java/com/nordigy/testrestapi/RestApiTests.java
+++ src/test/java/com/nordigy/testrestapi/RestApiTests.java
@@ -35,121 +35,116 @@ class RestApiTests {
         RestAssured.port = port;
     }
 
-        @Test
-        public void shouldReturnCorrectUsersListSize() {
-            given().log().all()
-                    .when().get("/api/users")
-                    .then().log().ifValidationFails()
-                    .statusCode(200)
-                    .body("page.totalElements", is(20));
-        }
+    @Test
+    public void shouldReturnCorrectUsersListSize() {
+        given().log().all()
+                .when().get("/api/users")
+                .then().log().ifValidationFails()
+                .statusCode(200)
+                .body("page.totalElements", is(20));
+    }
 
-        @Test
-        public void shouldReturnUserIfExists() {
-            ObjectNode user = given().log().all()
-                    .when().get("/api/users/10")
-                    .then().log().ifValidationFails()
-                    .statusCode(200)
-                    .extract().body().as(ObjectNode.class);
-            assertThat(user.get("id").asInt()).isEqualTo(10);
-        }
+    @Test
+    public void shouldCreateNewUser() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        objectNode.put("firstName", "Ivan");
+        objectNode.put("lastName", "Ivanov");
+        objectNode.put("dayOfBirth", "2000-01-01");
+        objectNode.put("email", "asdas@asdas.tr");
 
-        @Test
-        public void shouldNotReturnUserIfNotExists() {
-            given().log().all()
-                    .when().get("/api/users/30")
-                    .then().log().ifValidationFails()
-                    .statusCode(404);
-        }
+        ObjectNode user = given().log().all()
+                .body(objectNode)
+                .contentType(ContentType.JSON)
+                .when().post("/api/users")
+                .then().log().ifValidationFails()
+                .statusCode(201)
+                .extract().body().as(ObjectNode.class);
 
-        @Test
-        public void shouldSortByFirstNameForward() {
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectNode objectNode = objectMapper.createObjectNode();
-            objectNode.put("firstName", "Aaaaaaaaaaaaaaa");
-            objectNode.put("lastName", "Ив");
-            objectNode.put("dayOfBirth", "2000-01-01");
-            objectNode.put("email", "asdas@asdas.tr");
+        assertThat(user.get("id").asLong()).isGreaterThan(20);
+        assertThat(user.get("firstName")).isEqualTo(objectNode.get("firstName"));
+        assertThat(user.get("lastName")).isEqualTo(objectNode.get("lastName"));
+        assertThat(user.get("dayOfBirth")).isEqualTo(objectNode.get("dayOfBirth"));
+        assertThat(user.get("email")).isEqualTo(objectNode.get("email"));
+    }
 
-            given().log().all()
-                    .body(objectNode)
-                    .contentType(ContentType.JSON)
-                    .when().post("/api/users")
-                    .then().log().ifValidationFails()
-                    .statusCode(201);
+    @Test
+    public void shouldReturnUserIfExists() {
+        ObjectNode user = given().log().all()
+                .when().get("/api/users/10")
+                .then().log().ifValidationFails()
+                .statusCode(200)
+                .extract().body().as(ObjectNode.class);
+        assertThat(user.get("id").asInt()).isEqualTo(10);
+    }
 
-            given().log().all()
-                    .when().get("/api/users/?sort=firstName,asc")
-                    .then().log().ifValidationFails()
-                    .statusCode(200)
-                    .body(("_embedded.users.firstName.get(0)"), is("Aaaaaaaaaaaaaaa"));
-        }
+    @Test
+    public void shouldPutNewInfoToExistingUser() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        objectNode.put("firstName", "Julia");
+        objectNode.put("lastName", "Smith");
+        objectNode.put("dayOfBirth", "1967-01-01");
+        objectNode.put("email", "juli@asdas.ru");
 
-        @Test
-        public void shouldSortByFirstNameBackward() {
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectNode objectNode = objectMapper.createObjectNode();
-            objectNode.put("firstName", "Яяяяяяяяяяяяяяя");
-            objectNode.put("lastName", "Киселёв-Лопатин");
-            objectNode.put("dayOfBirth", "2000-01-01");
-            objectNode.put("email", "zzz@asdas.tr");
+        ObjectNode user = given().log().all()
+                .body(objectNode)
+                .contentType(ContentType.JSON)
+                .when().put("/api/users/3")
+                .then().log().ifValidationFails()
+                .statusCode(200)
+                .extract().body().as(ObjectNode.class);
 
-            given().log().all()
-                    .body(objectNode)
-                    .contentType(ContentType.JSON)
-                    .when().post("/api/users")
-                    .then().log().ifValidationFails()
-                    .statusCode(201);
+        assertThat(user.get("id").asInt()).isEqualTo(3);
+        assertThat(user.get("firstName")).isEqualTo(objectNode.get("firstName"));
+        assertThat(user.get("lastName")).isEqualTo(objectNode.get("lastName"));
+        assertThat(user.get("dayOfBirth")).isEqualTo(objectNode.get("dayOfBirth"));
+        assertThat(user.get("email")).isEqualTo(objectNode.get("email"));
+    }
 
-            given().log().all()
-                    .when().get("/api/users/?sort=firstName,desc")
-                    .then().log().ifValidationFails()
-                    .statusCode(200)
-                    .body(("_embedded.users.firstName.get(0)"), is("Яяяяяяяяяяяяяяя"));
-        }
+    @Test
+    public void shouldPatchNewInfoToExistingUser() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        objectNode.put("firstName", "Ani");
+        objectNode.put("email", "ani@asdas.ru");
 
-        @Test
-        public void shouldDeleteExistingUser() {
-            given().log().all()
-                    .when().delete("/api/users/1")
-                    .then().log().ifValidationFails()
-                    .statusCode(204);
-            given().log().all()
-                    .when().get("/api/users")
-                    .then().log().ifValidationFails()
-                    .statusCode(200)
-                    .body("page.totalElements", is(19));
-        }
+        ObjectNode userBeforePatch = given().log().all()
+                .body(objectNode)
+                .contentType(ContentType.JSON)
+                .when().get("/api/users/12")
+                .then().log().ifValidationFails()
+                .statusCode(200)
+                .extract().body().as(ObjectNode.class);
 
-        @Test
-        public void shouldPatchNewInfoToExistingUser() {
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectNode objectNode = objectMapper.createObjectNode();
-            objectNode.put("firstName", "Ani");
-            objectNode.put("email", "ani@asdas.ru");
+        ObjectNode userAfterPatch = given().log().all()
+                .body(objectNode)
+                .contentType(ContentType.JSON)
+                .when().patch("/api/users/12")
+                .then().log().ifValidationFails()
+                .statusCode(200)
+                .extract().body().as(ObjectNode.class);
 
-            ObjectNode userBeforePatch = given().log().all()
-                    .body(objectNode)
-                    .contentType(ContentType.JSON)
-                    .when().get("/api/users/12")
-                    .then().log().ifValidationFails()
-                    .statusCode(200)
-                    .extract().body().as(ObjectNode.class);
+        assertThat(userAfterPatch.get("id").asInt()).isEqualTo(12);
+        assertThat(userAfterPatch.get("firstName")).isEqualTo(objectNode.get("firstName"));
+        assertThat(userAfterPatch.get("lastName")).isEqualTo(userBeforePatch.get("lastName"));
+        assertThat(userAfterPatch.get("dayOfBirth")).isEqualTo(userBeforePatch.get("dayOfBirth"));
+        assertThat(userAfterPatch.get("email")).isEqualTo(objectNode.get("email"));
+    }
 
-            ObjectNode userAfterPatch = given().log().all()
-                    .body(objectNode)
-                    .contentType(ContentType.JSON)
-                    .when().patch("/api/users/12")
-                    .then().log().ifValidationFails()
-                    .statusCode(200)
-                    .extract().body().as(ObjectNode.class);
+    @Test
+    public void shouldDeleteExistingUser() {
+        given().log().all()
+                .when().delete("/api/users/1")
+                .then().log().ifValidationFails()
+                .statusCode(204);
+        given().log().all()
+                .when().get("/api/users")
+                .then().log().ifValidationFails()
+                .statusCode(200)
+                .body("page.totalElements", is(19));
+    }
 
-            assertThat(userAfterPatch.get("id").asInt()).isEqualTo(12);
-            assertThat(userAfterPatch.get("firstName")).isEqualTo(objectNode.get("firstName"));
-            assertThat(userAfterPatch.get("lastName")).isEqualTo(userBeforePatch.get("lastName"));
-            assertThat(userAfterPatch.get("dayOfBirth")).isEqualTo(userBeforePatch.get("dayOfBirth"));
-            assertThat(userAfterPatch.get("email")).isEqualTo(objectNode.get("email"));
-        }
     // TODO: The test methods above are examples of test cases.
     //  Please add new cases below, but don't hesitate to refactor the whole class.
 }
